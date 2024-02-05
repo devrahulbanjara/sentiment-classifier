c9abe783bf33590a29b5be660291ca77d89122ee
Rahul Dev Banjara <rdbanjara07@gmail.com>
2024-02-05 21:06:09 +0545

    updated

---
+++ 3.py
@@ -0,0 +1,16 @@
+def file_handling_example(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+
+        with open(file_path, 'w') as write_file:
+            write_file.write("Trying to write.")
+
+        with open(file_path, 'r') as eof_file:
+            while eof_file.read(1024):
+                pass
+
+    except (FileNotFoundError, PermissionError, EOFError) as e:
+        print(f"Error: {e}")
+
+file_handling_example('your_file_path/your_file.txt')
