99577c8ff927684425a209679737b4f22c1f18e3
Rahul Dev Banjara <rdbanjara07@gmail.com>
2024-03-15 13:06:28 +0545

    removed uncecessary files

--- sentiment_classifier_1.py
+++
@@ -1,9 +0,0 @@
-def strip_punctuation(string_to_remove_punctuation):
-    punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
-    punctuation_removed = string_to_remove_punctuation
-    for char in string_to_remove_punctuation:
-        if char in punctuation_chars:
-            punctuation_removed = punctuation_removed.replace(char, '')
-    return punctuation_removed
-
-
--- test.py
+++
@@ -1,5 +0,0 @@
-old_list=[1,2,3]
-
-new_list=list(map(lambda x:x**3,old_list))
-
-print(new_list)
